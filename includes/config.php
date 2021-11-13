10e45b123f3af377b621ad271f461cca6ef8da13
Александр <sh889@ya.ru>
2021-11-13 21:14:11 +0500

    День 1. Главная страница

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SITE_NAME', 'Twitter');
+define('HOST', 'http://' . $_SERVER['HTTP_HOST']);
+define('DB_HOST', 'twitter');
+define('DB_NAME', 'twitter');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
